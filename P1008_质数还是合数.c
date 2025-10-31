dc7c926635b1f55497705bc15d3722ca8d85604a
solitaire <codexepsilon@outlook.com>
2025-10-31 21:42:44 +0800

    Add prime number check in P1008_质数还是合数.c

---
+++ P1008_质数还是合数.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	for (i = 2; i <= sqrt(n); i++) 
+	{
+		if (n % i == 0) 
+		{
+			printf("No\n");
+			break;
+		}
+	}
+	if (i>=sqrt(n))
+	{
+		printf("Yes\n");
+	}
+	return 0;
+}
