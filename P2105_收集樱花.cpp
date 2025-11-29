5acba969d79c93eaaa49b88ab1bb6720bcaf1764
solitaire <codexepsilon@outlook.com>
2025-11-29 11:08:15 +0800

    Refactor main function for clarity and efficiency

--- P2105_收集樱花.cpp
+++ P2105_收集樱花.cpp
@@ -2,31 +2,25 @@
 using namespace std;
 //模拟（穷举）法：两层循环，寻找从i开始不大于V的最大值。时间复杂度为O(n^2)。
 //贪心策略:设置滑动窗口，若未超过总量则尽可能向右扩张，否则左边界右移。时间复杂度O(n).
-int main()
+int main() 
 {
-  int n,V,i,l=0,r=0;
-	int curv=0,maxv=0;
-	cin>>n>>V;
-	vector<int> a(n);
-	for(i=0;i<n;i++)
+    int n, V;
+    cin >> n >> V;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int l = 0, curv = 0, maxv = 0;
+    for (int r = 0; r < n; r++) 
 	{
-		cin>>a[i];
-	}
-	while(true)
-	{
-		if(curv<=V)
+        curv += a[r];
+        while (curv > V && l <= r) 
 		{
-			maxv=max(curv,maxv);
-			r++;
-			if(r==n) break;
-			curv=curv+a[r];
-		}
-		else
-		{
-			l++;
-			curv=curv-a[l];
-		}
-	}
-	cout<<maxv;
-	return 0;
+            curv -= a[l];
+            l++;
+        }
+        if (curv <= V)
+            maxv = max(maxv, curv);
+    }
+    cout << maxv;
+    return 0;
 }
