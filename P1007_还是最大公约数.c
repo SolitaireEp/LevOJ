2f77b4964cfafe53c15c9e32212b9abae95562d8
solitaire <codexepsilon@outlook.com>
2026-03-22 19:48:45 +0800

    Implement GCD calculation using Euclidean algorithm

--- P1007_还是最大公约数.c
+++ P1007_还是最大公约数.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int gcd(int x, int y) //欧几里得算法
+{
+    int t;
+    while (y!=0) 
+    {
+        int t = y;
+        y = x % y;
+        x = t;
+    }
+    return x;
+}
 
+int main()
+{
+    int n,i,a,b,c,r;
+	scanf("%d", &n);
+    for (i=0;i<n;i++)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+        r = gcd(gcd(a,b),c);
+        printf("%d\n",r);
+    }
+    return 0;
+}
