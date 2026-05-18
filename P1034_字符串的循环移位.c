74edf5b41408ea5221215c2194071044dca74a62
solitaire <codexepsilon@outlook.com>
2026-05-18 14:28:12 +0800

    Update P1034_字符串的循环移位.c

--- P1034_字符串的循环移位.c
+++ P1034_字符串的循环移位.c
@@ -4,12 +4,12 @@ int main()
 {
  char s[101] = {0};
  char output[101] = {0};
- int i,k,len; 
+ int i,k,len;
  scanf("%d",&k);
  scanf("%s",s);
  len = strlen(s);
  for (i=0;i<len;i++)
- output[i] = s[(i+k)%len]; //评测数据有大写字符；一次移位，无需k次循环
+  output[i] = s[(i+k)%len]; //评测数据有大写字符；一次移位，无需k次循环
  printf("%s",output);
  return 0;
 }
